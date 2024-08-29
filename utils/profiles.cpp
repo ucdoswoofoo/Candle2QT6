cdcf2b772680da953d7b2bf4df73957332f3aa3f
张丙军 <48965256+ucdoswoofoo@users.noreply.github.com>
2024-08-29 08:07:44 +0800

    Add files via upload

---
+++ utils/profiles.cpp
@@ -0,0 +1,34 @@
+#include "profiles.h"
+#include <QUrl>
+
+// chosen for backward compatibility with pre-profiles versions, which always stored settings in `settings.ini`
+const QString default_profile_name = "settings";
+
+QStringList getProfileNames()
+{
+    const QDir configDir = QStandardPaths::writableLocation(QStandardPaths::AppConfigLocation);
+    const QStringList configFiles = configDir.entryList({"*.ini"}, QDir::Files);
+    QStringList rv;
+    for (QString configFile : configFiles)
+    {
+        //rv << QUrl::fromPercentEncoding(configFile.replace(QRegExp("\\.ini$"), "").toUtf8());
+
+        rv << QUrl::fromPercentEncoding(configFile.replace(("\\.ini$"), "").toUtf8());
+
+        //QRegularExpression
+    }
+    return rv;
+}
+
+QString configPathForProfile(QString profile_name)
+{
+    std::string configPath = QStandardPaths::writableLocation(QStandardPaths::AppConfigLocation).toStdString();
+    auto pos = configPath.rfind('/');
+    if (pos != std::string::npos)
+    {
+        configPath.erase(pos);
+    }
+    QString tmp = QString::fromStdString(configPath) + "/Candle2";
+    QDir configDir(tmp);
+    return configDir.filePath(QUrl::toPercentEncoding(profile_name) + ".ini");
+}
